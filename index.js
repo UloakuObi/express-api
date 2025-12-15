fcace547d88b2511ff4e5c5cac53ec505fcf5d8d
UloakuObi <140077049+UloakuObi@users.noreply.github.com>
2025-12-15 22:02:07 +0000

    Update API

--- index.js
+++ index.js
@@ -8,17 +8,24 @@ const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 3000;
 
-const supabaseUrl = process.env.DATABASE_URL
-const supabaseKey = process.env.DATABASE_KEY
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 app.use(cors({
-    origin: '*' // for testing only; in production, restrict to your frontend URL
+    origin: '*', // for testing only; in production, restrict to your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }))
+
 
 app.use(express.json())
 
 // Routes
+
+// Test Route
+app.get("/ping", (req, res) => res.send("pong"));
+
 // Get data from db
 app.get("/people2", async(req, res) => {
     const {data, error} = await supabase.from("people2").select("*")
@@ -51,8 +58,9 @@ app.post("/people2", async (req, res) => {
         return res.status(500).json({Error: "Failed to post data"})
     }
 
-    res.status(201).json(data)
+    res.status(201).json({ message: "Data successfully posted" });
 })
+
 
 // Listening to port
 app.listen(port, () => {
